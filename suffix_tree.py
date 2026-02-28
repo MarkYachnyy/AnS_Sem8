7186a4b3fc656703dda0ce016758f95e49e0e534
MarkYachnyy <markyachny@gmail.com>
2026-02-28 23:56:35 +0300

    All 5 algos

--- suffix_tree.py
+++ suffix_tree.py
@@ -24,6 +24,54 @@ class Node:
         self.arc_in = None
 
 
+class VerboseListReadOnly(list):
+    """
+    Версия только для чтения - выводит информацию только при получении элементов.
+    """
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            start = key.start if key.start is not None else 0
+            stop = key.stop if key.stop is not None else len(self)
+            step = key.step if key.step is not None else 1
+            #print(f"обращение к массиву {self} по срезу [{start}:{stop}:{step}]")
+            result = super().__getitem__(key)
+            return VerboseListReadOnly(result)
+        else:
+            #print(f"обращение к массиву {self} по индексу {key}")
+            result = super().__getitem__(key)
+            return result
+
+
+def print_tree_with_string(node, s, level=0):
+    if node is None:
+        return
+
+    indent = "  " * level
+    if level == 0:
+        print(f"{indent}Корневой узел:")
+    else:
+        print(f"{indent}Узел (s_ref: {id(node.s_ref) if node.s_ref else None}):")
+
+    for i, arc in enumerate(node.arcs):
+        if arc is not None:
+            # Получаем фактическую подстроку для дуги
+            if arc.i_end == -1:  # До конца строки
+                substring = s[arc.i_beg:]
+                arc_info = f"[{arc.i_beg}, конец] = '{substring}'"
+            else:
+                substring = s[arc.i_beg:arc.i_end + 1]
+                arc_info = f"[{arc.i_beg}, {arc.i_end}] = '{substring}'"
+
+            print(f"{indent}  Дуга {i}: {arc_info} -> ", end="")
+
+            if arc.dest_vert is not None:
+                print(f"узел {id(arc.dest_vert)}")
+                print_tree_with_string(arc.dest_vert, s, level + 1)
+            else:
+                print("терминальный узел")
+
+
 def init_vert(nA, arc_in):
     res = Node(nA)
     res.arc_in = arc_in
@@ -41,8 +89,8 @@ def init_arc(src_node: Node = None, ch_arc_code=0, i_beg=-1, i_end=-1, dest_vert
     return arc
 
 
-def find_suffix_tree_arc(str, substr, m_same, tree):
-    m = len(substr)
+def find_suffix_tree_arc(str, substr, m, m_same, tree):
+    print("start find_suffix_tree_arc, параметры:", 'str:', str, 'substr:', substr, 'm:', m, 'm_same:', m_same, 'tree:', tree)
     arc = None
     idx_substr, idx_arc = 0, 0
     curr_node = tree
@@ -76,12 +124,14 @@ def find_suffix_tree_arc(str, substr, m_same, tree):
             stopped = True
     # if idx_substr == m:
     #     idx_arc += 1
+    #print("end find_suffix_tree_arc")
     return arc, idx_substr, idx_arc
 
 
-def top_jump_bottom(str, substr, arc, i_arc_end, idx_substr, idx_arc):
-    m = len(substr)
+def top_jump_bottom(str, substr, m, arc, i_arc_end, idx_substr, idx_arc):
+    #print("start top_jump_bottom, параметры:", 'str:', str, 'substr:',substr, 'm:', m, 'arc:', arc, 'i_arc_end:', i_arc_end, 'idx_substr:', idx_substr, 'idx_arc:', idx_arc)
     if not arc:
+        #print("end top_jump_bottom")
         return None, idx_substr, idx_arc
     arc_next = None
     src_vert = None
@@ -99,7 +149,8 @@ def top_jump_bottom(str, substr, arc, i_arc_end, idx_substr, idx_arc):
     if not src_vert.s_ref:
         n_chars_up -= 1
     n_vert_chr = m - n_chars_up
-    arc_next, idx_substr, idx_arc = find_suffix_tree_arc(str, substr[n_vert_chr:], n_chars_up-1, ref_vert)
+    arc_next, idx_substr, idx_arc = find_suffix_tree_arc(str, substr[n_vert_chr:n_vert_chr+n_chars_up], n_chars_up,
+                                                         n_chars_up - 1, ref_vert)
 
     if not arc_next:
         arc_next = ref_vert.arc_in
@@ -107,6 +158,7 @@ def top_jump_bottom(str, substr, arc, i_arc_end, idx_substr, idx_arc):
             idx_arc = arc_next.i_end + 1
 
     idx_substr += n_vert_chr
+    #print("end top_jump_bottom")
     return arc_next, idx_substr, idx_arc
 
 
@@ -114,25 +166,27 @@ def st_build_online_n2(str, nA):
     n = len(str)
     tree = init_vert(nA=nA, arc_in=None)
     init_arc(src_node=tree, ch_arc_code=str[0], i_beg=0, i_end=0, dest_vert=None, i_dest_vert=0)
-    for i in range(n):
+    for i in range(1, n):
         arc_prev: Arc = None
         i_end_prev = -1
         ref_from = None
         idx_substr = 0
         idx_arc = 0
         for j in range(i + 1):
+            print('i =', i, 'j =', j)
             m = i - j + 1
-            if j:
-                uv_arc, idx_substr, idx_arc = top_jump_bottom(str, str[j:], arc_prev, i_end_prev, idx_substr, idx_arc)
+            if j > 0:
+                uv_arc, idx_substr, idx_arc = top_jump_bottom(str, str[j:j+m], m, arc_prev, i_end_prev, idx_substr,
+                                                              idx_arc)
             else:
-                uv_arc, idx_substr, idx_arc = find_suffix_tree_arc(str, str[j:], m-1, tree)
+                uv_arc, idx_substr, idx_arc = find_suffix_tree_arc(str, str[j:j+m], m, m - 1, tree)
             arc_prev = uv_arc
             i_end_prev = arc_prev.i_end if arc_prev else -1
             if idx_substr == m:
                 if ref_from:
                     ref_from.s_ref = uv_arc.src_vert
                 ref_from = None
-                idx_arc-=1
+                idx_arc -= 1
                 continue
             w_node = None
             if not uv_arc:
@@ -152,7 +206,7 @@ def st_build_online_n2(str, nA):
 
                 uv_arc.dest_vert = w_node
                 uv_arc.i_dest_vert = -1
-                uv_arc.i_end = idx_arc-1
+                uv_arc.i_end = idx_arc - 1
 
                 if ref_from:
                     ref_from.s_ref = w_node
@@ -165,83 +219,18 @@ def st_build_online_n2(str, nA):
 
             init_arc(w_node, str[i], i, i, None, j)
 
+        #print_tree_with_string(tree, s)
+
     return tree
 
-s = 'ABRACADABRA$'
-int_str = str_to_int(s, 'ABCDR$')
-
-tree = st_build_online_n2(int_str, 6)
 
 
-def print_tree(node, level=0, label_prefix=""):
-    """
-    Рекурсивно печатает суффиксное дерево, начиная с указанного узла.
 
-    Args:
-        node: начальный узел для печати
-        level: уровень вложенности (для отступов)
-        label_prefix: префикс метки от корня до текущего узла
-    """
-    if node is None:
-        return
+s = 'ABAAB$'
+a = 'ABR$'
+int_str = str_to_int(s, a)
 
-    # Печатаем информацию о текущем узле
-    indent = "  " * level
-    if level == 0:
-        print(f"{indent}Корневой узел:")
-    else:
-        print(f"{indent}Узел (s_ref: {id(node.s_ref) if node.s_ref else None}):")
-
-    # Проходим по всем дугам из текущего узла
-    for i, arc in enumerate(node.arcs):
-        if arc is not None:
-            # Формируем информацию о дуге
-            arc_info = f"[{arc.i_beg}, {arc.i_end}]"
-
-            # Печатаем дугу
-            print(f"{indent}  Дуга {i}: {arc_info} -> ", end="")
-
-            if arc.dest_vert is not None:
-                print(f"узел {id(arc.dest_vert)}")
-                # Рекурсивно печатаем поддерево
-                new_prefix = label_prefix + arc_info
-                print_tree(arc.dest_vert, level + 1, new_prefix)
-            else:
-                print("терминальный узел")
+tree = st_build_online_n2(VerboseListReadOnly(int_str), len(a))
 
 
-def print_tree_with_string(node, s, level=0):
-    """
-    Печатает суффиксное дерево с отображением фактических подстрок.
-
-    Args:
-        node: начальный узел для печати
-        s: исходная строка, по которой построено дерево
-        level: уровень вложенности (для отступов)
-    """
-    if node is None:
-        return
-
-    indent = "  " * level
-    if level == 0:
-        print(f"{indent}Корневой узел:")
-    else:
-        print(f"{indent}Узел (s_ref: {id(node.s_ref) if node.s_ref else None}):")
-
-    for i, arc in enumerate(node.arcs):
-        if arc is not None:
-            # Получаем фактическую подстроку для дуги
-            if arc.i_end == -1:  # До конца строки
-                substring = s[arc.i_beg:]
-                arc_info = f"[{arc.i_beg}, конец] = '{substring}'"
-            else:
-                substring = s[arc.i_beg:arc.i_end + 1]
-                arc_info = f"[{arc.i_beg}, {arc.i_end}] = '{substring}'"
-
-            print(f"{indent}  Дуга {i}: {arc_info} -> ", end="")
-
-            if arc.dest_vert is not None:
-                print(f"узел {id(arc.dest_vert)}")
-                print_tree_with_string(arc.dest_vert, s, level + 1)
-            else:
-                print("терминальный узел")
+print_tree_with_string(tree, s)
